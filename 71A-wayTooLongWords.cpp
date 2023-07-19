fb4be44dd5b110b4c51c0e9d095f86d81b2597ce
PrachetShah <prachetshah25@gmail.com>
2023-07-19 10:59:29 +0530

    solved 71A

---
+++ 71A-wayTooLongWords.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    vector<string> words(t);
+    for(int i=0; i<t; i++){
+        cin >> words[i];
+    }
+
+    for(int i=0; i<words.size(); i++){
+        int n = words[i].length();
+        if(n > 10){
+            words[i] = words[i][0] + to_string(n-2) + words[i][n-1];
+            cout << words[i] << endl;;
+        }else{
+            cout << words[i] << endl;
+        }
+    }
+
+    return 0;
+}
