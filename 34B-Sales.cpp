ee0598e04f8c6c688601f25d590990186707e1ca
PrachetShah <prachetshah25@gmail.com>
2023-07-22 21:35:48 +0530

    34B Sales Completed

---
+++ 34B-Sales.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<int> prices(n);
+    for(int i=0; i<n; i++){
+        cin >> prices[i];
+    }
+
+    // for(int i=0; i<n; i++){
+    //     cout << prices[i] << " ";
+    // }
+    // cout << endl;
+
+    sort(prices.begin(), prices.end());
+
+    int ans = 0;
+    for(int i=0; i<m; i++){
+        // cout << i << " " << prices[i] << endl; 
+        if(prices[i] > 0){
+            break;
+        }
+        ans += -prices[i];
+    }
+    cout << ans << endl;
+
+    return 0;
+}
