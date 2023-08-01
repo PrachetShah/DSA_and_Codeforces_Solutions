93518de57a7731f6f80758ffe7e9603b73b37c48
PrachetShah <prachetshah25@gmail.com>
2023-08-01 18:10:57 +0530

    266A Solved

---
+++ 115A-Party.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> sup(n);
+    for(int i=0; i<n; i++){
+        cin >> sup[i];
+    }
+
+    
+    return 0;   
+}
