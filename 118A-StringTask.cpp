eed6b4d1240674a87056a2c57d7f10d755c328a4
PrachetShah <prachetshah25@gmail.com>
2023-08-03 18:16:14 +0530

    118A String Task Done

---
+++ 118A-StringTask.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string input;
+    cin >> input;
+
+    string ans;
+
+    for(int i=0; i < input.length(); i++){
+        if(input[i] >= 65 && input[i] <= 90){
+            input[i] = input[i] + 32;
+        }
+        if(input[i]=='a' || input[i]=='i' || input[i]=='e' || input[i]=='o' || input[i]=='u' || input[i]=='y'){
+            continue;
+        }else{
+            ans += '.';
+            ans += input[i];
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
+}
