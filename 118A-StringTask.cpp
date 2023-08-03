de6e7c1d9bff209ee8ba84f5587a1502990fa13b
PrachetShah <prachetshah25@gmail.com>
2023-08-03 20:26:53 +0530

    118A String Done

--- 118A-StringTask.cpp
+++ 118A-StringTask.cpp
@@ -9,9 +9,11 @@ int main(){
     string ans;
 
     for(int i=0; i < input.length(); i++){
+        // Char is uppercase 
         if(input[i] >= 65 && input[i] <= 90){
             input[i] = input[i] + 32;
         }
+        // Char is from the given VOWELS list -> a e i o u y
         if(input[i]=='a' || input[i]=='i' || input[i]=='e' || input[i]=='o' || input[i]=='u' || input[i]=='y'){
             continue;
         }else{
