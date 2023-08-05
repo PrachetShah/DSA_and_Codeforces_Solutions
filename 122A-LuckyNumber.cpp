8a4f7b83232dbae6ef3cc072f14a8cd63a9753ba
PrachetShah <prachetshah25@gmail.com>
2023-08-05 09:42:56 +0530

    122A Lucky Number Solved

---
+++ 122A-LuckyNumber.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> nums = {4, 7, 47, 77, 447, 474, 477, 744 , 747, 777};
+    bool flag = true;
+    for(int i=0; i<10; i++){
+        if(n < nums[i]){
+            break;
+        }
+        if(n%nums[i] == 0){
+            cout << "YES" << endl;
+            flag = false;
+            break;
+        }
+    }
+
+    if(flag){
+        cout << "NO" << endl;
+    }
+    return 0;
+}
