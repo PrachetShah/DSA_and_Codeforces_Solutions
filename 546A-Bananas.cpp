def1dc852a116bbaa74cdf0c953fc2faf5995d50
PrachetShah <prachetshah25@gmail.com>
2023-08-12 12:55:28 +0530

    546A Solved

---
+++ 546A-Bananas.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    long long int k,n,w;
+    cin >> k >> n >> w;
+
+    long long req = k*(w*(w+1)/2);
+    if(req <= n){
+        cout << 0 << endl;
+    }else{
+        cout << req-n << endl;
+    }
+
+    return 0;
+}
