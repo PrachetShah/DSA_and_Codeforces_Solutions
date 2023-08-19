0c0ea1cd116acee7e4acf28c6c2ce2d951b3b3c3
PrachetShah <prachetshah25@gmail.com>
2023-08-19 14:23:18 +0530

    1857A Array Coloring Done

---
+++ 1857A-ArrayColoring.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        
+        
+        int cntOdd = 0;
+
+        for(int i=0; i<n; i++){
+            int x;
+            cin >> x;
+            if(x%2 != 0)
+                cntOdd++;
+        }
+
+        if(cntOdd%2 == 0){
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" << endl;
+        }
+    }
+}
