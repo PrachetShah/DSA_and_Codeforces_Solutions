98abcbb503d06b65a06f246be89ab6b257829ff6
PrachetShah <prachetshah25@gmail.com>
2023-08-27 10:20:48 +0530

    734A Anton and Danik Done

---
+++ 734A-AntonAndDanik.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    int countA=0, countD=0;
+    for(int i=0; i<s.length(); i++){
+        if(s[i]=='A')
+            countA++;
+        else 
+            countD++;
+    }
+
+    if(countA > countD){
+        cout << "Anton";
+    }else if(countA < countD){
+        cout << "Danik";
+    }else{
+        cout << "Friendship";
+    }
+
+    return 0;
+}
