f38f8487cf08baf32d6e104c07f48803f8deb291
PrachetShah <prachetshah25@gmail.com>
2023-09-25 22:48:24 +0530

    41A Translation Done

---
+++ 41A-Translation.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s,t;
+    cin >> s;
+    cin >> t;
+
+    reverse(s.begin(), s.end());
+    bool flag = true;
+    
+    for(int i=0; i<s.length(); i++){
+        if(s[i] != t[i]){
+            cout << "NO" << endl;
+            flag = false;
+            break;
+        }
+    }
+
+    if(flag){
+        cout << "YES" << endl;
+    }
+}
