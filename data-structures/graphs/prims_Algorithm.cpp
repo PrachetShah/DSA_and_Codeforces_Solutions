5f7c27e461786546ab959c870ae9a71a543c59ea
PrachetShah <prachetshah25@gmail.com>
2023-12-24 17:25:33 +0530

    added theory for spanning tree and MST

---
+++ data-structures/graphs/prims_Algorithm.cpp
@@ -0,0 +1,10 @@
+/*
+Spanning Tree: https://www.geeksforgeeks.org/spanning-tree/
+1. A Spanning tree does not exist for a disconnected graph.
+2. For a connected graph having 'N' vertices then the number of edges in the spanning tree for that graph will be 'N-1'.
+3. A Spanning tree does not have any cycle.
+
+Minimum Spanning Tree(MST):
+A minimum spanning tree (MST) is defined as a spanning tree that has the minimum weight among all the possible spanning trees.
+To find MST, we can make use of Prim's or Kruskal's Algorithm.
+*/
