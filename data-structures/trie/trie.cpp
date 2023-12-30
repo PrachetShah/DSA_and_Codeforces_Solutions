fe07a9355125d91e2908f31393955e00ab3fd2fe
PrachetShah <prachetshah25@gmail.com>
2023-12-30 12:41:09 +0530

    theory for trie data structure

--- data-structures/trie/trie.cpp
+++ data-structures/trie/trie.cpp
@@ -1,3 +1,30 @@
+/*
+Trie Data Structure
+Trie is a type of k-ary search tree used for storing and searching a specific key from a set. Using Trie, search 
+complexities can be brought to optimal limit (key length). 
+
+Advantages of tries
+1. In tries the keys are searched using common prefixes. Hence it is faster. The lookup of keys depends upon the 
+height in case of binary search tree. 
+2. Tries take less space when they contain a large number of short strings. As nodes are shared between the keys.
+3. Tries help with longest prefix matching, when we want to find the key.
+
+Comparison of tries with hash table
+1. Looking up data in a trie is faster in worst case as compared to imperfect hash table.
+2. There are no collisions of different keys in a trie.
+3. In trie if single key is associated with more than one value then it resembles buckets in hash table.
+4. There is no hash function in trie.
+5. Sometimes data retrieval from tries is very much slower than hashing.
+6. Representation of keys a string is complex. For example, representing floating point numbers using strings 
+is really complicated in tries.
+7. Tries always take more space than hash tables.
+
+Applications of tries
+1. Tries has an ability to insert, delete or search for the entries. Hence they are used in building dictionaries 
+such as entries for telephone numbers, English words.
+2. Tries are also used in spell-checking softwares.
+*/
+
 #include<bits/stdc++.h>
 
 using namespace std;
