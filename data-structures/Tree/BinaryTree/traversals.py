0d6e19a66137bdd57461f62090446343e156e39e
PrachetShah <prachetshah25@gmail.com>
2025-01-03 15:00:38 +0530

    zig zag traversals

--- data-structures/Tree/BinaryTree/traversals.py
+++ data-structures/Tree/BinaryTree/traversals.py
@@ -1,6 +1,28 @@
 from implementation import Tree, Node
 from collections import defaultdict
 
+def zigZagTraversal(root):
+    queue = []
+    queue.append([root])
+    isEven = 0
+    ans = []
+    while queue:
+        curr = queue.pop()
+        nextDepth = []
+        for node in curr:
+            if node.left:
+                nextDepth.append(node.left)
+            if node.right:
+                nextDepth.append(node.right)
+        if isEven%2==0:
+            ans.append([node.data for node in curr])
+        else:
+            ans.append([node.data for node in curr[::-1]])
+        isEven = not isEven
+        if len(nextDepth):
+            queue.append(nextDepth)
+    print("Zig Zag Traversal:", ans)
+
 def verticalTraversal(root):
     map = defaultdict(list)
     def helper(root, index, map):
@@ -29,8 +51,11 @@ if __name__ == "__main__":
     root.right.right = Node(7)
     root.right.left.right = Node(8)
     root.right.right.right = Node(9)
+    root.right.right.right.right = Node(100)
+    root.right.right.right.left = Node(120)
 
     tree = Tree()
     tree.levelOrderTraversal(root)
 
+    zigZagTraversal(root)
     verticalTraversal(root)
