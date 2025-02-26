41279a0cc2e5cafa91e7f69372cd29b9f72f44e2
PrachetShah <prachetshah25@gmail.com>
2025-02-26 18:42:22 +0530

    two knights and two sets solution explained

--- usaco_guide/cses Problem Set/twoKnights.cpp
+++ usaco_guide/cses Problem Set/twoKnights.cpp
@@ -1,3 +1,27 @@
+/*
+Let's solve this problem for a particular K X K chessboard. Total number of squares in K X K chessboard is K2. 
+So, number of ways the first knight can be placed is K2. Now the number of ways the second knight can be placed will be 
+K2 - 1. This leads us to, the total number of ways the two knights can be placed in K X K chessboard is K2 * (K2-1)/2,
+we divide by 2 because both knights are identical. Now we need to subtract number of ways the two knights attack each 
+other from above equation.
+
+For this we need to observe that the two knights attack each other when they are in a 2 X 3 or 3 X 2 block. 
+The number of ways two knights can attack each other in a 2 X 3 block is 2 and number of ways two knights can attack 
+each other in a 3 X 2 block is 2
+
+Now, we just need to find the total number of 2 X 3 and 3 X 2 blocks in a K X K chessboard.  In a K X K chessboard, 
+the 2*3 blocks can be arranged  in (K-1) rows (starting from the first row till the (K-1)-th row), and there are (K-2)
+ways to position them in columns (starting from the first column till the (K-2)-th column) which gives us 
+(K-1)*(K-2) ways to place a 2 X 3 block in a K X K chessboard. Similarly, we can place a 3 X 2 block in (K - 1) * (K - 2) ways. 
+
+So, the total number of ways two knights can attack each other in a K X K chessboard will be sum of 2 * number of 2 X 3 blocks 
+and 2 * number of 3 X 2 blocks, which is 4 * (K - 1) * (K - 2). It is every important to note that no 2 ways of attacking are 
+same in the derived formula.
+
+Hence the total number of ways Two Knights can be placed in a K X K chessboard such that they don't attack each other is:
+
+(K2 * (K2 - 1)) / 2 - (4 * (K - 1) * (K - 2)) 
+*/
 #include<bits/stdc++.h>
 
 using namespace std;
--- usaco_guide/cses Problem Set/twoSets.cpp
+++ usaco_guide/cses Problem Set/twoSets.cpp
@@ -1,3 +1,13 @@
+/*
+To be able to divide the set into two different sets of equal sum, n must be of the form 4k or 4k+3,
+
+case [i] -> n = 4k put first n/4 numbers and last n/4 in first set, and remaining n/2 numbers in second set, for eg, n= 8 {1,2,7,8}, {3,4,5,6}
+
+case [ii] -> n = 4k + 3
+
+observation- 4n 4n+1 4n+2 4n+3, then 4n + 4n+3 == 4n + 1 + 4n + 2, therefore make partition accordingly.
+*/
+
 #include<bits/stdc++.h>
 
 using namespace std;
