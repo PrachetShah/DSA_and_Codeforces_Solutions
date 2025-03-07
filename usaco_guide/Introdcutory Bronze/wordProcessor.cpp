969512b91147c549f56897804d5e3fb49322cfed
PrachetShah <prachetshah25@gmail.com>
2025-03-07 11:58:22 +0530

    2 solutions for word processor

--- usaco_guide/Introdcutory Bronze/wordProcessor.cpp
+++ usaco_guide/Introdcutory Bronze/wordProcessor.cpp
@@ -21,11 +21,15 @@ void tc(){
     int n,k;
     cin >> n >> k;
     vector<string> essay;
-
+    
+    /*
+     * SOLUTION 1
+    */
     string word;
     int charCount = 0;
     for(int i=0; i<n; i++){
         cin >> word;
+        essay.push_back(word); // for Solution 2
         
         charCount += word.length();
         if(charCount <= k){
@@ -38,6 +42,31 @@ void tc(){
             charCount = word.length();
             cout << "\n" << word;
         }
+    }
+
+    /*
+     * SOLUTION 2
+    */
+    int charCount = 0;
+    string line = "";
+    for(auto word: essay){
+        if(charCount + word.length() > k){
+            if(line.length()>0){
+                cout << line << "\n";
+            }
+            charCount = word.length();
+            line = word;
+        }else{
+            charCount += word.length();
+            if(line.length() > 0){
+                line += " " + word;
+            }else{
+                line = word;
+            }
+        }
+    }
+    if(line.length() >= 0){
+        cout << line;
     }
 }
 
