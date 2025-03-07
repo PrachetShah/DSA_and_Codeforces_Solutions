72d43b8ccab0a6fcf8ebcaae6c530842df5ed639
PrachetShah <prachetshah25@gmail.com>
2025-03-07 11:14:53 +0530

    Promotion Counting Usaco

--- usaco_guide/Introdcutory Bronze/promotionCounting.cpp
+++ usaco_guide/Introdcutory Bronze/promotionCounting.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+#define MOD 1000000007;
+
+using ll = long long;
+using vll = vector <ll>;
+using ii = pair <ll, ll>;
+using vii = vector <ii>;
+
 // the argument is the input filename without the extension
 void setIO(string s) {
 	freopen((s + ".in").c_str(), "r", stdin);
@@ -10,17 +17,30 @@ void setIO(string s) {
 }
 
 void tc(){
+    ll bronzeBefore, bronzeAfter;
+    cin >> bronzeBefore >> bronzeAfter;
 
+    ll silverBefore, silverAfter;
+    cin >> silverBefore >> silverAfter;
+
+    ll goldBefore, goldAfter;
+    cin >> goldBefore >> goldAfter;
+
+    ll platBefore, platAfter;
+    cin >> platBefore >> platAfter;
+
+    // start from end till you find not same
+    ll goldToPlat = platAfter - platBefore;  
+    ll silverToGold = goldAfter - goldBefore + goldToPlat;
+    ll bronzeToSilver = silverAfter - silverBefore + silverToGold;
+    
+    cout << bronzeToSilver << endl;
+    cout << silverToGold << endl;
+    cout << goldToPlat << endl;
 }
 
 int main() {
 	setIO("promote");
 
-    int a,b,x,y;
-    cin >> a >> b >> x >> y;
-
-    int dist = abs(b-a);
-    dist = min(dist, abs(a-x) + abs(b-y));
-    dist = min(dist, abs(a-y) + abs(b-x));
-    cout << dist;
+    tc();
 }
