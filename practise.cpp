bab3f17cc75cab77004982ee63ded2cb6ab8c652
PrachetShah <prachetshah25@gmail.com>
2025-03-10 23:14:19 +0530

    geline use

--- practise.cpp
+++ practise.cpp
@@ -9,30 +9,8 @@ int main(){
     cin.tie(NULL);
 
     int t;
-    cin >> t;
-
-    while(t--){
-        ll x, y;
-        cin >> y >> x;
-
-        if(y>x){
-            ll ans = (y-1)*(y-1);
-            ll add = 0;
-            if(y%2!=0){
-                add = x;
-            }else{
-                add = 2*y-x;
-            }
-            cout << ans + add << "\n";
-        }else{
-            ll ans = (x-1)*(x-1);
-            ll add = 0;
-            if(x%2==0){
-                add = y;
-            }else{
-                add = 2*x-y;
-            }
-            cout << ans + add << "\n";
-        }
-    }
+    // cin >> t;to take whitespace input in c++
+    string name;
+    getline(cin >> ws, name);
+    cout << name;
 }
