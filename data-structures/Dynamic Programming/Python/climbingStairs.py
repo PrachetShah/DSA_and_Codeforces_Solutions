f11169209ac62e9d39c01678a494d2986dbca170
PrachetShah <prachetshah25@gmail.com>
2025-03-12 23:37:30 +0530

    space optimised house robber using fibonacci

--- data-structures/Dynamic Programming/Python/climbingStairs.py
+++ data-structures/Dynamic Programming/Python/climbingStairs.py
@@ -9,5 +9,17 @@ def climbStairs(n):
         return dp[n]
     return helper(n)
 
+def climbSP(n):
+    first = 0
+    second = 1
+    for i in range(n):
+        sum = first + second
+        first, second = second, sum
+    return sum
+
 print("Num Ways of climbing stairs for n=5:", climbStairs(5))
+print("Num Ways of climbing stairs for n=3:", climbSP(5))
 print("Num Ways of climbing stairs for n=3:", climbStairs(3))
+print("Num Ways of climbing stairs for n=3:", climbSP(3))
+print("Num Ways of climbing stairs for n=3:", climbStairs(30))
+print("Num Ways of climbing stairs for n=3:", climbSP(30))
