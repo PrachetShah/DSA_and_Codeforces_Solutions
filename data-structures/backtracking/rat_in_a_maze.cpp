e2a07163775475a92fa64631d1b8486f589e93e7
PrachetShah <prachetshah25@gmail.com>
2025-06-01 12:26:13 +0530

    rat in a maze refactor

--- data-structures/backtracking/rat_in_a_maze.cpp
+++ data-structures/backtracking/rat_in_a_maze.cpp
@@ -13,7 +13,7 @@ vector < vector < int >> & arr, int n){
 }
 
 
-void solve(int x, int y, vector < vector < int >> & arr, int n, vector<string> &ans, 
+void solve(int x, int y, vector<vector<int>> &arr, int n, vector<string> &ans, 
 vector<vector<bool>> & vis, string path){
     // base case
     if(x==n-1 && y==n-1){
@@ -44,7 +44,7 @@ vector<vector<bool>> & vis, string path){
     }
     vis[x][y] = 0;
 }
-vector < string > searchMaze(vector < vector < int >> & arr, int n) {
+vector<string> searchMaze(vector<vector<int>> &arr, int n) {
     // Write your code here.
     vector<string> ans;
     vector<vector<bool>> visited(n, vector<bool> (n, 0));
@@ -56,4 +56,8 @@ vector < string > searchMaze(vector < vector < int >> & arr, int n) {
 
     solve(0,0,arr,n,ans,visited,path);
     return ans;
+}
+
+int main(){
+    return 0;
 }
