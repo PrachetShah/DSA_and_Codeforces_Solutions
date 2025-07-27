c2fd388f7129e15cc789cb7c4366ad28bd5ea572
PrachetShah <prachetshah25@gmail.com>
2025-07-27 12:30:46 +0530

    implementation of row wise sorted matrix median

--- data-structures/Binary Search/BS_27-MedianOfRowMatrix.cpp
+++ data-structures/Binary Search/BS_27-MedianOfRowMatrix.cpp
@@ -2,3 +2,56 @@
 
 using namespace std;
 
+class Solution {
+  private:
+    int upperBound(vector<int> arr, int toSearch, int m){
+        int ans = m;
+        int low = 0;
+        int high = arr.size()-1;
+        
+        while(low <= high){
+            int mid = (high+low)/2;
+            if(arr[mid] > toSearch){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+  
+    int countSmallEqual(vector<vector<int>> &mat, int toSearch, int n, int m){
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            cnt += upperBound(mat[i], toSearch, m);
+        }
+        return cnt;
+    }
+  public:
+    int median(vector<vector<int>> &mat) {
+        // code here
+        int n = mat.size();
+        int m = mat[0].size();
+        int req = (n*m)/2;
+        
+        int low = INT_MAX;
+        int high = INT_MIN;
+        
+        for(int i=0; i<n; i++){
+            low = min(low, mat[i][0]); 
+            high = max(high, mat[i][m-1]); 
+        }
+        
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            int smallEqual = countSmallEqual(mat, mid, n, m);
+            if(smallEqual <= req){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return low;
+    }
+};
