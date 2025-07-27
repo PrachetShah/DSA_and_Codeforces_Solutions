bf1cf9c08f9c947bc2c5597ab3752e4c5f6c7b03
PrachetShah <prachetshah25@gmail.com>
2025-07-27 12:18:23 +0530

    lower and upper bound in binary search

---
+++ data-structures/Binary Search/BS_2-Lower_and_UpperBound.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int lowerBound(vector<int> arr, int toSearch){
+    int n = arr.size();
+    int ans = 0;
+    int low = 0, high=n-1;
+
+    while(low <= high){
+        int mid = (low+high)/2;
+        if(arr[mid] >= toSearch){
+            ans = mid;
+            high = mid-1;
+        }else{
+            low = mid+1;
+        }
+    }
+    return ans;
+}
+
+int upperBound(vector<int> arr, int x){
+    int n = arr.size();
+    int ans = 0;
+    int low = 0, high=n-1;
+    while(low <= high){
+        int mid = (low+high)/2;
+        if(arr[mid] > x){
+            ans = mid;
+            high = mid-1;
+        }else{
+            low = mid+1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr = {1,2,3,3,7,8,9,9,9,11};
+    int toSearch = 9;
+    cout << arr.size() << endl;
+    cout << lowerBound(arr, toSearch) << endl;
+    cout << upperBound(arr, toSearch) << endl;
+}
